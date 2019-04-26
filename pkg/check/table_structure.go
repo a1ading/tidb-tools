7da7c6bc4202f4c6136caf2bb7af2bfdd7e3ca93
a1ading <komoz@qq.com>
2019-04-26 10:03:38 +0800

    column mapping: if schema or table didn't contain an id, use 0 as default

--- pkg/check/table_structure.go
+++ pkg/check/table_structure.go
@@ -20,7 +20,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/pingcap/tidb-tools/pkg/column-mapping"
+	"github.com/a1ading/tidb-tools/pkg/column-mapping"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/parser"
